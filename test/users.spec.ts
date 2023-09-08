49b9089a328cee62310c19b90e101a3152fdfa6d
GianDutra <chikegang@gmail.com>
2023-09-08 15:53:57 -0300

    add: metrics test

--- test/users.spec.ts
+++ test/users.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeEach, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeEach, beforeAll, describe, it, expect } from 'vitest'
 import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
@@ -24,4 +24,75 @@ describe('Users routes', () => {
       .send({ name: 'john doe', email: 'johndoe@gmail.com' })
       .expect(201)
   })
-})
+
+  it('should be possible to retrieve user metrics', async () => {
+    const createUsersResponse = await request(app.server)
+      .post('/users')
+      .send({ name: 'john doe', email: 'johndoe@gmail.com' })
+      .expect(201)
+
+      const cookies = createUsersResponse.get('Set-Cookie')
+
+      const listUsersReponse = await request(app.server)
+      .get('/users')
+      .set('Cookie', cookies)
+      
+
+        // Pega o ID do usuário criado
+      const user_id = listUsersReponse.body[0].id;
+
+      
+
+      await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({ name: "Comida da manha", description: "Sanduiche com suco de laranja", on_diet: true, user_id: user_id })
+      
+
+      await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({ name: "Comida da tarde", description: "Cookies com Coca cola", on_diet: false, user_id: user_id })
+      
+
+      await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({ name: "Comida da noite", description: "Legumes e peixes", on_diet: true, user_id: user_id })
+      
+
+      await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({ name: "Comida da madrugada", description: "Melancia e banana", on_diet: true, user_id: user_id })
+      
+
+      //apenas para verificar se as refeições estão chegando
+      const listMealsReponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      
+
+      // Requisição para pegar métricas
+    const listMetricsResponse = await request(app.server)
+    .get(`/users/${user_id}/metrics`)
+    .set('Cookie', cookies)
+    
+
+
+    // Verificação das métricas
+    expect(listMetricsResponse.body.metrics).toEqual({
+      "totalMealsCount": 4,
+      "mealsOnDietCount": 3,
+      "mealsOffDietCount": 1,
+      "bestSequenceOfMealsOnDiet": 2
+    });
+});
+
+
+
+
+
+
+    
+  })
