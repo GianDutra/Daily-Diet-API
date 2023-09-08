74385c6a49e2d7f78e269ba7e4c23d87a8e6a866
GianDutra <chikegang@gmail.com>
2023-09-08 14:37:23 -0300

    add: start creating users.spec.test

---
+++ test/users.spec.ts
@@ -0,0 +1,27 @@
+import { afterAll, beforeEach, beforeAll, describe, it } from 'vitest'
+import { execSync } from 'node:child_process'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Users routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+
+  it('should be able to create a new user', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({ name: 'john doe', email: 'johndoe@gmail.com' })
+      .expect(201)
+  })
+})
